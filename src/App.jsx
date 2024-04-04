6d264fb7e867ed8b97a7625de706fc24d1f98343
santaimany <140429236+santaimany@users.noreply.github.com>
2024-04-04 15:10:02 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center gap-4 flex-col min-h-screen">
+      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
+    </main>
+  );
+}
+
+export default App;
